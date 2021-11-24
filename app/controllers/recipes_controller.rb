c2bce5f3487477f486c54a1cf3dfd626d2f6ec23
mn <marvoosh@gmail.com>
2021-11-24 13:05:40 +0000

    search with select 2 is working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,17 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
+    @ingredients = ingredients_collection
+    @ingredient_ids = params[:query][:ingredient_ids]
   end
-
 
   def show
     @recipe = Recipe.find(params[:id])
   end
 
+  private
 
+  def ingredients_collection
+    Ingredient.all.map { |ingredient| [ingredient.id, ingredient.name] }
+  end
 end
