5f1a2879a3fde34140577b1742807ddcf1015294
Thallia Borba Rosa Salbego <thalliaborbarosa@gmail.com>
2021-11-24 12:14:19 +0000

    new model validations and relations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
 
   include PgSearch::Model
   pg_search_scope :search_by_name,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :saved_recipes
+  has_many :recipes, through: :saved_recipes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
