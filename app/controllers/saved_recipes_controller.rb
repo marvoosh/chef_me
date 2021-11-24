1e28829b2e6a28240d57af8f999d33a2308e24fb
Thallia Borba Rosa Salbego <thalliaborbarosa@gmail.com>
2021-11-24 13:02:40 +0000

    working on the dashboard

---
+++ app/controllers/saved_recipes_controller.rb
@@ -0,0 +1,5 @@
+class SavedRecipesController < ApplicationController
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :recipes, only: [:index, :show]
+  get 'dashboard', to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'search', to: 'pages#search'
 end
