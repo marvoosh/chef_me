59cfe3f2d4968782cf3ef40de1b9b1ec1ec0f6c5
Thallia Borba Rosa Salbego <thalliaborbarosa@gmail.com>
2021-11-26 12:04:00 +0000

    new card with shadow and icons

--- app/controllers/saved_recipes_controller.rb
+++ app/controllers/saved_recipes_controller.rb
@@ -1,7 +1,11 @@
 class SavedRecipesController < ApplicationController
   def create
-    @saved_recipe = SavedRecipe.new
     @recipe = Recipe.find(params[:recipe_id])
+    @saved_recipe = SavedRecipe.find_or_create_by(recipe: @recipe, user: current_user) do |saved_recipe|
+      if params[:attribute].present?
+        saved_recipe.toggle!(params[:attribute])
+      end
+    end
     @saved_recipe.recipe = @recipe
     @saved_recipe.user = current_user
     @saved_recipe.save
@@ -11,12 +15,6 @@ class SavedRecipesController < ApplicationController
   def destroy
     @saved_recipe = SavedRecipe.find(params[:id])
     @saved_recipe.destroy
-    redirect_to recipes_path
-  end
-
-  def update
-    @saved_recipe = SavedRecipe.find(params[:id])
-    @saved_recipe.toggle!(params[:attribute])
     redirect_to recipes_path
   end
 end
