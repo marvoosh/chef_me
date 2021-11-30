24d10bf9fedacdee8d557dde18d223430f32134b
mn <marvoosh@gmail.com>
2021-11-30 12:59:27 +0000

    almost there

--- app/javascript/controllers/card-icons_controller.js
+++ app/javascript/controllers/card-icons_controller.js
@@ -13,6 +13,9 @@ export default class extends Controller {
         headers: {
           'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
         },
+        body: {
+          'favourite': true
+        },
         method: 'POST'
       })
         .then(r=>r.json())
@@ -29,6 +32,56 @@ export default class extends Controller {
       e.target.dataset.saved = '0'
       e.target.classList.remove('fas')
       e.target.classList.add('far')
+      // let favourite = document.querySelector('.fa-heart')
+      let favourite = e.currentTarget.parentElement.children[1].children[0]
+      console.log(favourite)
+      let complete = e.currentTarget.parentElement.children[2].children[0]
+      console.log(complete)
+      if (favourite.classList.contains('fas')) {
+        favourite.classList.remove('fas')
+        favourite.classList.add('far')
+      }
+      if (complete.classList.contains('fas')) {
+        complete.classList.remove('fas')
+        complete.classList.add('far')
+      }
+
+
+    }
+  }
+
+  favourite(e) {
+    let bookmark = e.currentTarget.parentElement.previousElementSibling
+    if (e.currentTarget.classList.contains('fas')) {
+      // console.log(e.currentTarget.parentElement.previousElementSibling)
+        e.currentTarget.classList.remove('fas')
+        e.currentTarget.classList.add('far')
+    } else {
+      // console.log(e.currentTarget.parentElement.previousElementSibling)
+      e.currentTarget.classList.remove('far')
+      e.currentTarget.classList.add('fas')
+      if (bookmark.classList.contains('far')) {
+        bookmark.classList.remove('far')
+        bookmark.classList.add('fas')
+      }
+    }
+  }
+
+  complete(e) {
+    let bookmark = e.currentTarget.parentElement.parentElement.children[0]
+    console.log(bookmark)
+    if (e.currentTarget.classList.contains('fas')) {
+      // console.log(e.currentTarget.parentElement.previousElementSibling)
+      e.currentTarget.classList.remove('fas')
+      e.currentTarget.classList.add('far')
+    } else {
+      // console.log(e.currentTarget.parentElement.previousElementSibling)
+      e.currentTarget.classList.remove('far')
+      e.currentTarget.classList.add('fas')
+      if (bookmark.classList.contains('far')) {
+        bookmark.classList.remove('far')
+        bookmark.classList.add('fas')
+      }
     }
   }
 }
